7db21b6903be0469e6352c7be1296ab9b05c042c
Oxsadik <torikussadik404@gmail.com>
2026-09-11 17:13:19 +0600

    customer router and more

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.database import Base, engine
 
-from app.routers import products
+from app.routers import products, customers
 
 
 app = FastAPI(title="Outbound delivery Tracker API")
@@ -27,3 +27,4 @@ def health():
 
 
 app.include_router(products.router)
+app.include_router(customers.router)
